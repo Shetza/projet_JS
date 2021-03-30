43fdcb043b089f77f5a79e14d776be8d14c1b4ad
Karl <karlhinterseber1@gmail.com>
2021-03-30 15:30:36 +0200

    feat formulaire fixes #11

--- crypto.js
+++ crypto.js
@@ -1,32 +1,32 @@
 //feed
-$(function(){
+$(document).ready(function () {
     $.ajax({
-        url: 'https://api.n.exchange/en/api/v1/currency/', 
-        method: 'GET', 
-        dataType : 'json' 
-    }).done(function(crypto){    
-        crypto.forEach(element => {
-            if ( element.code === "BTC") {
+        url: 'https://api.n.exchange/en/api/v1/currency/',
+        method: 'GET',
+        dataType: 'json'
+    }).done(function (reponse) {
+        reponse.forEach(element => {
+            if (element.code === "BTC") {
                 const divbtc = document.querySelector("#outputbtc")
                 divbtc.innerText = element.name
-            } else if ( element.code === "ETH") {
+            } else if (element.code === "ETH") {
                 const divbtc = document.querySelector("#outputeth")
                 divbtc.innerText = element.name
-            } else if ( element.code === "XMR") {
+            } else if (element.code === "XMR") {
                 const divbtc = document.querySelector("#outputxmr")
                 divbtc.innerText = element.name
-            } else if ( element.code === "XVG") {
+            } else if (element.code === "XVG") {
                 const divbtc = document.querySelector("#outputxvg")
                 divbtc.innerText = element.name
             }
         })
     })
-    .fail(function(erreur){
-        alert("La requete a échoué" + JSON.stringify(erreur));
-    })
-    .always(function(){
-        alert("Requête effectuée");
-    });
+        .fail(function (erreur) {
+            console.log("La requete a échoué" + JSON.stringify(erreur));
+        })
+        .always(function () {
+            console.log("Requête effectuée");
+        });
 });
 
 //dropdown
@@ -34,18 +34,30 @@ $(function(){
 //quand l'utilisateur clique sur le bouton, le .toggle montre le contenu du menu
 function functionDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // ferme le dropdown quand l'utilisateur clique en dehors du bouton
-  window.onclick = function(event) {
+}
+
+// ferme le dropdown quand l'utilisateur clique en dehors du bouton
+window.onclick = function (event) {
     if (!event.target.matches('.dropbtn')) {
-      let dropdowns = document.getElementsByClassName("dropdown-content");
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        let dropdowns = document.getElementsByClassName("dropdown-content");
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            let openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  }
+}
+
+//formulaire
+
+function getValue() {
+    // Sélectionner l'élément input et récupérer sa valeur
+    let recuptext = document.getElementById("in").value;
+    // Afficher la valeur
+    //alert(recuptext);
+    let b =document.body; 
+    b.append(recuptext);
+}
+
